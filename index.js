56e471c1ae1cd07100a6b17d5dd08a15b20601ae
Michael Martin <michael@problogdesign.com>
2018-03-11 16:14:54 +0000

    Hello World of Puppeteer

---
+++ index.js
@@ -0,0 +1,9 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://forum.fangathering.com");
+
+  //   await browser.close();
+})();
