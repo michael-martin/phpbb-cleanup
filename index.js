9e6d993ecbdceb65a0c14c4e5e16f9c65bb673f3
Michael Martin <michael@problogdesign.com>
2018-03-11 16:20:05 +0000

    Read .env config values

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://forum.fangathering.com");
+  await page.goto(process.env.FORUM_URL);
 
   //   await browser.close();
 })();
