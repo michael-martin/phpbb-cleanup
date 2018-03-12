6bebb9b7e29c8e5561b44fcd8681837ca9e8fadc
Michael Martin <michael@problogdesign.com>
2018-03-12 15:48:04 +0000

    Confirm deletions and report totals.

--- selectors.js
+++ selectors.js
@@ -11,7 +11,8 @@ const SELECTORS = {
   POST_COUNT: "#count",
   DELETE_POSTS: 'input[name="deleteposts"][value="1"]',
   DELETE_USERS: 'input[name="action"][value="delete"]',
-  PRUNE_SUBMIT: '#acp_prune input[type="submit"]'
+  PRUNE_SUBMIT: '#acp_prune input[type="submit"][name="update"]',
+  PRUNE_CONFIRM_SUBMIT: '#confirm input[type="submit"][name="confirm"]'
 };
 
 module.exports = SELECTORS;
